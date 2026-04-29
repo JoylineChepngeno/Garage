ce8e8f5387bcb769fe5a1675f8a0a2c205c413fe
Joyline Chepngeno <joylinechep001@gmail.com>
2026-04-29 13:10:02 +0200

    feat(auth): toggle forgot password UI to show reset link confirmation

--- src/app/features/auth/forgotpassword/forgotpassword.ts
+++ src/app/features/auth/forgotpassword/forgotpassword.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgotpassword',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './forgotpassword.html',
   styleUrl: './forgotpassword.css',
 })
-export class Forgotpassword {}
+export class Forgotpassword {
+  private router = inject(Router);
+showmessage = false;
+
+  onSendResetLink(){
+    console.log("clicked")
+  
+    this.showmessage = true;
+  
+  }
+  checkNextPage(){
+    console.log("clicked")
+    debugger;
+    this.router.navigate(['/reset'])
+    
+
+  }
+
+
+}
