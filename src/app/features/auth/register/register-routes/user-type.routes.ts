ca9d1e00af3f89f969ae6107913ba97a20c4283a
Joyline <joylinechep001@gmail.com>
2026-05-07 11:13:06 +0300

    refactor(auth): move register routes into a folder on its on

---
+++ src/app/features/auth/register/register-routes/user-type.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from "@angular/router"
+import { UserType } from "../user-type/user-type"
+import { CarOwner } from "../car-owner/car-owner"
+import { GarageAdmin } from "../garage-admin/garage-admin"
+import { VerifyOTP } from "../veryfyOTP/verify-otp"
+
+export const routes: Routes = [
+    // Load a specific child by default when navigating to parent
+    // Add an empty path redirect
+
+    {path: '',
+        component: UserType,
+        children: [
+            {path: 'car-owner', component: CarOwner },
+            {path: 'garage-admin', component: GarageAdmin},
+            {path: 'verifyotp',component: VerifyOTP}
+        ]
+
+    }
+]
